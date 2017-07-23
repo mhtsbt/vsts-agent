82875df6728a1e9b3c56810037c2516996384de0
Matthias Hutsebaut <matthias@MatthiasBook.local>
2017-07-23 18:06:23 +0200

    added start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+./bin/Agent.Listener configure --unattended \
+  --agent "${VSTS_AGENT:-$(hostname)}" \
+  --url "https://$VSTS_ACCOUNT.visualstudio.com" \
+  --auth PAT \
+  --token "$VSTS_TOKEN" \
+  --pool "${VSTS_POOL:-Default}" \
+  --replace & wait $!
+
+./bin/Agent.Listener run & wait $!
